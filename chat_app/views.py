e3309c8516b454ac5fdcaab0cb5fb129ed234157
Magdalena260 <magdalena.laurisch@outlook.de>
2026-09-18 11:19:05 +0200

    view.py chats data return

--- chat_app/views.py
+++ chat_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Chat
+
+
 
 # Create your views here.
+def chat_view(request):
+    if request.method == "GET":
+        chats = Chat.objects.all()
+        data = list(chats.values())
+        return JsonResponse(data, safe=False)
