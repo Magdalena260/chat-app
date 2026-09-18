defa14a677f3e5cddc92078c3a2d50fb19071f2b
Magdalena260 <magdalena.laurisch@outlook.de>
2026-09-18 11:25:12 +0200

    post

--- chat_app/views.py
+++ chat_app/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Chat
@@ -10,3 +12,12 @@ def chat_view(request):
         chats = Chat.objects.all()
         data = list(chats.values())
         return JsonResponse(data, safe=False)
+
+    def chat_view(request):
+    if request.method == "GET":
+        chats = Chat.objects.all()
+        data = list(chats.values())
+        return JsonResponse(data, safe=False)
+
+    if request.method == "POST":
+          data = json.loads(request.body)
